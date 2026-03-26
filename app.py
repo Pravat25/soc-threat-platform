c651a945b73c512c632cf7268e950137335e4cbd
Pravat25 <pravatdahal25@gmail.com>
2026-03-26 07:17:58 -0400

    Added log ingestion system to SOC platform

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def read_logs():
+    try:
+        with open("logs/sample.log", "r") as f:
+            logs = f.readlines()[::-1]  # latest first
+    except:
+        logs = ["No logs found"]
+
+    return logs
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    logs = read_logs()
+    return render_template("index.html", logs=logs)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
